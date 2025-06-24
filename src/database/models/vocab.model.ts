b04a407ead2db7f02e955e0c1c791e046981eb34
Adrish7 <adrishmajumder7@gmail.com>
2025-06-24 18:31:19 +0800

    Update

--- src/database/models/vocab.model.ts
+++ src/database/models/vocab.model.ts
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
 
 const vocabSchema = new mongoose.Schema({
-    word: {type: String, required:true},
-    definition: {type: String, required:true},
-})
+  word: { type: String, required: true },
+  definition: { type: String, required: true },
+});
 
-// Safety check (prevents re-compilation errors) || Core functionality (creates the model)
 const Vocab = mongoose.models.Vocab || mongoose.model("Vocab", vocabSchema);
 
 export default Vocab;
